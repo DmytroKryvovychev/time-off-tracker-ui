f3b987fbfd3713c44377ca2422a943a86be0658a
Dmytro Kryvovychev <nondemondias4x@gmail.com>
2020-11-24 15:00:21 +0200

    Updates

---
+++ precache-manifest.7cc854a799606313606e87fb002b8102.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e135d97ef8f7a005acd8cb8fe9c9e35",
+    "url": "/time-off-tracker-ui/index.html"
+  },
+  {
+    "revision": "561dc013ad3400a7f69e",
+    "url": "/time-off-tracker-ui/static/css/2.a688b289.chunk.css"
+  },
+  {
+    "revision": "a46d202011d47bb96814",
+    "url": "/time-off-tracker-ui/static/css/main.d7ed5022.chunk.css"
+  },
+  {
+    "revision": "561dc013ad3400a7f69e",
+    "url": "/time-off-tracker-ui/static/js/2.d0f981d6.chunk.js"
+  },
+  {
+    "revision": "c7b6e35e398934a3df0bb3c51997c639",
+    "url": "/time-off-tracker-ui/static/js/2.d0f981d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46d202011d47bb96814",
+    "url": "/time-off-tracker-ui/static/js/main.53598d46.chunk.js"
+  },
+  {
+    "revision": "778706329dfb43aaac5c",
+    "url": "/time-off-tracker-ui/static/js/runtime-main.42e713a4.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/time-off-tracker-ui/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/time-off-tracker-ui/static/media/getFetch.013f0b90.cjs"
+  }
+]);
